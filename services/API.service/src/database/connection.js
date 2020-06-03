ad5e992a3a652481603c31d54db0d8dfdb3e9be4
Joao Pinto <joaobose@gmail.com>
2020-06-03 09:54:59 -0400

    non global connection

---
+++ services/API.service/src/database/connection.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../../../../config/db.config.json')[env];
+
+module.exports = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
