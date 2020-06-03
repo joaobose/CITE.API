b898f3e769cc605ab84323b4f9a0955bbebecb66
Joao Pinto <joaobose@gmail.com>
2020-06-03 10:01:05 -0400

    back to global database connection

--- services/API.service/src/database/connection.js
+++ services/API.service/src/database/connection.js
@@ -2,9 +2,12 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../../../../config/db.config.json')[env];
 
-module.exports = new Sequelize(
+const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
   config
 );
+
+module.exports = sequelize;
+global.sequelize = sequelize;
