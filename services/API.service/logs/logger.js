a262a7d3750018d3be8209ecd173eaaaa2df7382
Joao Pinto <joaobose@gmail.com>
2020-06-04 03:04:09 -0400

    logger JSDoc

--- services/API.service/logs/logger.js
+++ services/API.service/logs/logger.js
@@ -10,11 +10,56 @@ const colors = {
   fgRed: '\x1b[31m'
 };
 
-// ------------------ logger singleton ------------------- //
+/**
+ * Customized Logger.
+ *
+ * This class is an enhancement of @code{console.log},
+ * it provides better logging and also support microservice logging
+ *
+ * @since      1.0
+ * @access     public
+ *
+ * @constructs Logger
+ */
 class Logger {
+  /**
+   * The service logging name
+   *
+   * @since  1.0
+   * @access public
+   *
+   * @type     {String}
+   *
+   * @member   {String} name
+   * @memberof Logger
+   */
   static name = 'server';
+
+  /**
+   * The winston logger instance.
+   *
+   * @since  1.0
+   * @access private
+   *
+   * @type     {winston.Logger}
+   *
+   * @member   {winston.Logger} logger
+   * @memberof Logger
+   */
   #logger = undefined;
 
+  /**
+   * Logger constructor.
+   *
+   * Constructs a logger and sets the service logger name if given.
+   *
+   * @since      1.0
+   * @access     public
+   *
+   * @constructs Logger
+   *
+   * @param {String} [name] The new service name.
+   */
   constructor(name) {
     this.logger = winston.createLogger({ format: winston.format.json() });
 
@@ -36,6 +81,15 @@ class Logger {
     }
   }
 
+  /**
+   * Writes a message into the info logging channel.
+   *
+   * @since      1.0
+   * @access     public
+   * @memberof   Logger
+   *
+   * @param {String} message The message to write.
+   */
   info(message) {
     if (env == 'test') return;
 
@@ -60,6 +114,15 @@ class Logger {
     });
   }
 
+  /**
+   * Writes an error into the error logging channel.
+   *
+   * @since      1.0
+   * @access     public
+   * @memberof   Logger
+   *
+   * @param {String} error The error to write.
+   */
   error(error) {
     if (env == 'test') return;
 
