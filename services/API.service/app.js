05e21ec42ee08de55271b34faa301549d94bd1b0
Joao Pinto <joaobose@gmail.com>
2020-12-07 01:43:50 -0400

    Adding docs/public to app.js

--- services/API.service/app.js
+++ services/API.service/app.js
@@ -28,6 +28,12 @@ app.use('/auth', authRouter);
 const userRouter = require('./src/routes/user.routes');
 app.use('/user', userRouter);
 
+// ------------------------- docs --------------------------- //
+// app.use('/', express.static(__dirname + '/apidoc'));
+
+// ------------------------ public -------------------------- //
+// app.use('/public/', express.static(__dirname + '/public'));
+
 // ------------------------ socket -------------------------- //
 const io = require('socket.io')(server);
 const Socket = require('fun.framework/classes/src/socket/Socket');
