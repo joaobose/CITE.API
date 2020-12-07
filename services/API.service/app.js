3dfd06ec12cf2261d7da00ea042cee7a41ac5c14
Joao Pinto <joaobose@gmail.com>
2020-12-07 02:29:45 -0400

    Adding API docs

--- services/API.service/app.js
+++ services/API.service/app.js
@@ -29,10 +29,10 @@ const userRouter = require('./src/routes/user.routes');
 app.use('/user', userRouter);
 
 // ------------------------- docs --------------------------- //
-// app.use('/', express.static(__dirname + '/apidoc'));
+app.use('/', express.static(__dirname + '/docs'));
 
 // ------------------------ public -------------------------- //
-// app.use('/public/', express.static(__dirname + '/public'));
+app.use('/public/', express.static(__dirname + '/public'));
 
 // ------------------------ socket -------------------------- //
 const io = require('socket.io')(server);
